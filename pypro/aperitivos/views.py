9055162936ff592e8e657338653cc29764f00686
Marqueane <marqueane_64@hotmail.com>
2022-08-13 18:42:17 -0300

    configuração para uso de filtro no título
    close #43

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def video(request, slug):
     videos = {
         'motivacao': {'titulo': 'Video Aperitivo: Motivacao'},
-        'instalacao-windows': {'titulo': 'Video Aperitivo: Instalacao Windows'},
+        'instalacao-windows': {'titulo': 'Instalacao Windows '},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
